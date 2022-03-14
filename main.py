7bef9703a6f138df579bf2a3390014c404b9d266
hsx <1025058409@qq.com>
2022-03-14 16:03:06 +0800

    git提交的测试2

--- main.py
+++ main.py
@@ -4,13 +4,24 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('world')
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('world')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+s = input("请输入你的字符串\n")
+
+result = []
+
+for i in range(0,len(s),2):
+    if i + 1 < len(s):
+        result.append(s[i]+s[i+1])
+    else:
+        result.append(s[i])
+for i in result:
+    print(i,end=' ')
